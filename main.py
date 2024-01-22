ee7f50a51c324bbe0df6f311657d35f70adfd18f
Matthew Brunner <MatthewBrunner>
2024-01-22 14:53:37 -0600

    Added get_num_characters function that takes a string as input and outputs a dictionary with counts of each character in input string

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_characters = get_num_characters(text)
     print(text)
     print(f"word count is: {num_words}")
+    print(num_characters)
 
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_characters(text):
+    character_dict = {}
+    text_lower = text.lower()
+    for character in text_lower:
+        if character not in character_dict:
+            character_dict[character] = 1
+        character_dict[character] += 1
+    return character_dict
+
 
 
 def get_book_text(path):
