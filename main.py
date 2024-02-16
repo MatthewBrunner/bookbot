700b63f332ed086c64d30fb08cdbde5aefd22df7
Matthew Brunner <MatthewBrunner>
2024-02-15 20:28:53 -0600

    fixed typo calling get_num_characters function

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    num_characters = get_num_characters(text)
+    character_dict = get_num_characters(text)
     print(text)
     print(f"word count is: {num_words}")
-    print(num_characters)
-
+    print(get_num_characters)
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
 
 def get_num_characters(text):
     character_dict = {}
@@ -21,7 +21,6 @@ def get_num_characters(text):
             character_dict[character] = 1
         character_dict[character] += 1
     return character_dict
-
 
 
 def get_book_text(path):
