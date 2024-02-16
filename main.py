d3e078e12479cd0c11b146a3b92dd68b59236645
Matthew Brunner <MatthewBrunner>
2024-02-15 23:41:56 -0600

    Added: final report, alpha only filter, conversion of dictionary of character and their occurence to a list of dictonaries with name key for character and num key for number of occurences.

--- main.py
+++ main.py
@@ -2,32 +2,43 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    character_dict = get_num_characters(text)
-    print(text)
-    print(f"word count is: {num_words}")
-    print(get_num_characters)
-
+    character_dict = get_char_dict(text)
+    list_of_dicts = dict_to_list_dict(character_dict)
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"Word count is: {num_words}.")
+    for dict in list_of_dicts:
+        print(f"The character {dict['name']} was found {dict['num']} times.")
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
     return len(words)
 
 
-def get_num_characters(text):
-    character_dict = {}
+def get_char_dict(text):
+    char_d = {}
     text_lower = text.lower()
     for character in text_lower:
-        if character not in character_dict:
-            character_dict[character] = 1
-        character_dict[character] += 1
-    return character_dict
-
+        if character in char_d:
+            char_d[character] += 1
+        else:
+            char_d[character] = 1
+    return char_d
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
-  
+
+def dict_to_list_dict(dict):
+   list_dict = []
+   for char in dict:
+       if char.isalpha():
+           new_dict = {'name': char, 'num': dict[char]}
+           list_dict.append(new_dict)
+   return list_dict
+
+def sort_on(dict):
+    return dict["num"]
+
 main()
-
-
