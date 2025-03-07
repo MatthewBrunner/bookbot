5e111731c5d19413bbbc0bb46118ba7df3dc2df0
Matthew Brunner <mpbrunner@protonmail.com>
2025-03-07 13:25:20 -0600

    Refactored to take book path as arg instead of being hard coded

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 def main():
+    import sys
     from stats import get_num_words, get_num_chars, get_list_dict
 
-    book_path = "books/frankenstein.txt"
+    arg_count = 0
+    for i in sys.argv:
+        arg_count += 1
+    if arg_count < 2:
+        print("Proper usage is: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars = get_num_chars(text)
